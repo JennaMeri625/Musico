2456934ecf8142d41c48535f9d97d7169f123731
JennaMeri625 <114081695+JennaMeri625@users.noreply.github.com>
2023-06-20 20:54:47 -0400

    created more Links between pages, added a LoggedInPage, ProfilePage

--- frontend/front/src/components/LogIn.tsx
+++ frontend/front/src/components/LogIn.tsx
@@ -1,8 +1,25 @@
+import { LinkContainer } from "react-router-bootstrap";
+
 
 export function LogIn() {
     return (
-    <div style={{padding: 100}}>
-        Log In Here!
-    </div>
+
+        <>
+        <div style={{marginLeft: 300, marginTop: 50}}>
+            <input type="email" placeholder="Enter Email" />
+            <input type="password" placeholder="Type Password" />
+            
+            <LinkContainer to={"/ProfilePage"}>
+            <button type="submit">Log In</button>
+            </LinkContainer>
+            
+        </div>
+        <div style={{ padding: 100 }}>
+
+            Need to add "Log in with Google"
+
+            onClick from login with Google should Link to="/LoggedInPage"
+        </div>
+        </>
     )
 };
---
+++ frontend/front/src/components/LoggedInPage.tsx
@@ -0,0 +1,29 @@
+import { Header } from "./Header";
+import { HomePage } from "./HomePage";
+import { LinkContainer } from "react-router-bootstrap";
+
+export function LoggedInPage() {
+    
+    return (
+
+        <>
+
+        <div style={{marginLeft: 475, marginTop: 100}}>Welcome "{"Name"}"! 
+        <div style={{marginLeft: 20, marginTop: 20}}>
+        <LinkContainer to={"/ProfilePage"}>
+        <button> Go To Profile </button>
+        </LinkContainer>
+        </div>
+        </div>
+
+        <HomePage />
+
+        <div style={{marginLeft: 500, marginTop: 30}}>
+        <LinkContainer to={"/HomePage"}>
+        <button> Sign Out </button> 
+        </LinkContainer>
+        </div>
+
+        </>
+    )
+}
