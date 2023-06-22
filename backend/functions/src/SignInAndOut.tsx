2e89239f3b3e52da1f83e225b218c55c3ad27e04
JennaMeri625 <114081695+JennaMeri625@users.noreply.github.com>
2023-06-21 23:18:42 -0400

    added backend folder and began the hosting on firebase

---
+++ backend/functions/src/SignInAndOut.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { signInWithGoogle, signOut } from "../../firebaseConfig";
+
+<><button onClick={signInWithGoogle}>Sign in with Google</button><button onClick={signOut}>Sign out</button></>
