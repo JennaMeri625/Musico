eb3e25c3d6ec1c7f38be9f950a9bdc4f4d400b91
JennaMeri625 <114081695+JennaMeri625@users.noreply.github.com>
2023-06-22 19:14:48 -0400

    adding backend files

---
+++ backend/firebaseConfig.ts
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAF9LdWa5i5YJskiQh6cYg_MBN4auveCMI",
+    authDomain: "musico-80127.firebaseapp.com",
+    projectId: "musico-80127",
+    storageBucket: "musico-80127.appspot.com",
+    messagingSenderId: "958687933218",
+    appId: "1:958687933218:web:297dbbc32ab0aa49502b7b",
+    measurementId: "G-XYTHRV3SY7"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  export const auth = getAuth(app);
+  const authProvider = new GoogleAuthProvider();
+
+  export function signInWithGoogle(): void {
+    signInWithPopup(auth, authProvider);
+  }
+
+  export function signOut(): void {
+    auth.signOut();
+  }
