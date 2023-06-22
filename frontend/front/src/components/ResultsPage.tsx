e17157f440f2e3b921b1ccaf7110e68e4157462d
Omar <omar.mahmoud32@gmail.com>
2023-06-22 19:26:25 -0400

    added maps api and result page

--- frontend/front/src/components/ResultsPage.tsx
+++ frontend/front/src/components/ResultsPage.tsx
@@ -1,9 +1,14 @@
+import React from "react";
+import MapComponent from "./Map";
 
 
 export function ResultsPage() {
     return (
     <div>
         These are the results!
+        <MapComponent />
     </div>
     )
+
 };
+
