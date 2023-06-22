428b0cd96e5dc051be10efadc4cf44e8d1bc7d77
trelaj <112722527+trelaj@users.noreply.github.com>
2023-06-22 01:44:49 -0400

    styling components and linked login page to loggedinpage

--- frontend/front/src/components/HomePage.tsx
+++ frontend/front/src/components/HomePage.tsx
@@ -1,9 +1,14 @@
-import { LinkContainer } from "react-router-bootstrap";
 import { useRef } from "react";
+
+
+
 
 export function HomePage() {
   const zipCodeRef = useRef<HTMLInputElement>(null);
   const keywordsRef = useRef<HTMLInputElement>(null);
+
+
+
 
   const handleKeyDown = (
     event: React.KeyboardEvent<HTMLInputElement>,
@@ -15,6 +20,9 @@ export function HomePage() {
     }
   };
 
+
+
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const zipCode = zipCodeRef.current?.value;
@@ -22,29 +30,43 @@ export function HomePage() {
     // Perform form submission logic using zipCode and keywords
     console.log("Searching with zip code:", zipCode);
     console.log("Searching with keywords:", keywords);
+
+
+
+
+    // Programmatically navigate to the ResultsPage
+    // Replace "/ResultsPage" with the correct path
+    window.location.href = "/ResultsPage";
   };
 
-  return (
-    <div style={{ paddingLeft: 350, paddingTop: 50 }}>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="zip code"
-          style={{ marginRight: 10 }}
-          ref={zipCodeRef}
-          onKeyDown={(e) => handleKeyDown(e, keywordsRef)}
-        />
-        <input
-          type="text"
-          placeholder="keywords: flute, wedding, singer, etc"
-          style={{ marginRight: 10 }}
-          ref={keywordsRef}
-          onKeyDown={(e) => handleKeyDown(e, zipCodeRef)}
-        />
 
-        <LinkContainer to={"/ResultsPage"}>
-          <button type="submit">Search</button>
-        </LinkContainer>
+
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <form onSubmit={handleSubmit}>
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", paddingTop: "200px" }}>
+          <input
+            type="text"
+            placeholder="zip code"
+            style={{ marginBottom: 10, border: "5px solid black", textAlign: "center" }}
+            ref={zipCodeRef}
+            onKeyDown={(e) => handleKeyDown(e, keywordsRef)}
+          />
+          <input
+            type="text"
+            placeholder="keywords: flute, wedding, singer, etc"
+            style={{ marginBottom: 10, border: "5px solid black", textAlign: "center", width: "300px" }}
+            ref={keywordsRef}
+            onKeyDown={(e) => handleKeyDown(e, zipCodeRef)}
+          />
+          <button
+            type="submit"
+            style={{ backgroundColor: "black", color: "white", fontWeight: "bold", padding: "10px 20px", border: "none" }}
+          >
+            Search
+          </button>
+        </div>
       </form>
     </div>
   );
--- frontend/front/src/components/LogIn.tsx
+++ frontend/front/src/components/LogIn.tsx
@@ -1,8 +1,69 @@
+import React, { useRef } from "react";
+import { LinkContainer } from "react-router-bootstrap";
 
-export function LogIn() {
-    return (
-    <div style={{padding: 100}}>
-        Log In Here!
+
+export const LogIn: React.FC = () => {
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+
+  const handleKeyDown = (
+    event: React.KeyboardEvent<HTMLInputElement>,
+    ref: React.RefObject<HTMLInputElement>
+  ) => {
+    if (event.key === "Tab" && ref.current) {
+      event.preventDefault();
+      ref.current.focus();
+    }
+  };
+
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const username = usernameRef.current?.value;
+    const password = passwordRef.current?.value;
+
+
+    // Perform login logic using username and password
+    console.log("Username:", username);
+    console.log("Password:", password);
+
+
+    // Redirect or perform further actions after login
+    // ...
+  };
+
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <form onSubmit={handleSubmit}>
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", paddingTop: "200px" }}>
+          <input
+            type="text"
+            placeholder="Username"
+            style={{ marginBottom: 10, border: "5px solid black", textAlign: "center" }}
+            ref={usernameRef}
+            onKeyDown={(e) => handleKeyDown(e, passwordRef)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            style={{ marginBottom: 10, border: "5px solid black", textAlign: "center" }}
+            ref={passwordRef}
+            onKeyDown={(e) => handleKeyDown(e, usernameRef)}
+          />
+           <LinkContainer to={"/ProfilePage"}>
+          <button
+            type="submit"
+            style={{ backgroundColor: "black", color: "white", fontWeight: "bold", padding: "10px 20px", border: "none" }}
+          >
+            Login
+          </button>
+          </LinkContainer>
+        </div>
+      </form>
     </div>
-    )
+  );
 };
+
+
