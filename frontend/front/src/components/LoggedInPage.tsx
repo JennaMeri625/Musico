1790a4db929dad54d01face7398f79164ab24cbb
JennaMeri625 <114081695+JennaMeri625@users.noreply.github.com>
2023-06-25 21:18:13 -0400

    completed LogInWithGoogle

--- frontend/front/src/components/LoggedInPage.tsx
+++ frontend/front/src/components/LoggedInPage.tsx
@@ -1,17 +1,21 @@
+import { useContext } from "react";
 import { signOut } from "../firebaseConfig";
+import AuthContext from "../services/AuthContext";
 import { HomePage } from "./HomePage";
 import { LinkContainer } from "react-router-bootstrap";
 
 
 export function LoggedInPage() {
    
+    const { user } = useContext(AuthContext);
+
     return (
 
 
         <>
+        <div style={{marginLeft: 30}}>Welcome {user!.displayName}!</div>
 
-
-        <div style={{marginLeft: 475, marginTop: 100}}>Welcome "{"Name"}"!
+        <div style={{marginLeft: 475, marginTop: 100}}>
         <div style={{marginLeft: 20, marginTop: 20}}>
         <LinkContainer to={"/ProfilePage"}>
         <button> Go To Profile </button>
